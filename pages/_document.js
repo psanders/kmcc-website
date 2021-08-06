f105752096e708d203e0119ca2bf2b7e2cd4d605
Pedro Sanders <sanderspedro@gmail.com>
2021-08-06 11:07:23 -0400

    Added gtag

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,21 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500&family=Source+Sans+Pro:wght@300;400;600&display=swap"
             rel="stylesheet"
           />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KN5HH9B7RN"
+        />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-KN5HH9B7RN');
+              `
+            }}
+          />
         </Head>
         <body>
           <Main />
